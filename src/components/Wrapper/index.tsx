b9302cb07b5ab53debf26a92311d17d456dfa265
Gabriel Rabelo <rabelo.g14@gmail.com>
2024-07-04 16:55:34 -0300

    fix: replace image props

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -5,7 +5,7 @@ type WrapperProps = ComponentProps<'div'>
 export function Wrapper(props: WrapperProps) {
   return (
     <div
-      className="lg:max-w-custom-screen mx-auto w-full max-w-sm"
+      className="mx-auto w-full max-w-sm lg:max-w-custom-screen"
       {...props}
     ></div>
   )
